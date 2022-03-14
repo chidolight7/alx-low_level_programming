ce270417c5948817cb779e27e9733d4e9ce4f56f
chidolight7 <light.odazie@yahoo.com>
2022-03-14 07:19:47 -0700

    task12

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,34 +1,50 @@
 #include <stdio.h>
 /**
- * main - Entry point
- *
- * Description: return all combination of 0 to 99
+ * main - main block
+ * Description: Print all possible combinations of two two-digit numbers.
+ * Numbers should range from 0 to 99.
+ * The two numbers should be separated by a space.
+ * All numbers should be printed with two digits. 1 should be printed as 01.
+ * Combination of numbers must be separated by a comma followed by a space.
+ * Combinations of numbers should be printed in ascending order.
+ * `00 01` and `01 00` are considered as the same combination.
+ * You can only use `putchar` to print to console.
+ * You can only use `putchar` up to 8 times.
+ * You are not allowed to use any variable of type `char`.
  * Return: 0
  */
 int main(void)
 {
-int n, m, d1, d2, d3, d4;
+int i, j;
+int a, b, c, d;
 
-for (n = 0; n <= 98; n++)
+for (i = 0; i < 100; i++)
 {
-for (m = n + 1; m <= 99; m++)
+a = i / 10; /* doubles fnum */
+b = i % 10; /* singles fnum */
+
+for (j = 0; j < 100; j++)
 {
-d1 = n / 10;
-d2 = n % 10;
-putchar(d1 + '0');
-putchar(d2 + '0');
-putchar(' ');
-d3 = m / 10;
-d4 = m % 10;
-putchar(d3 + '0');
-putchar(d4 + '0');
-if (n != 98 || m != 99)
+c = j / 10; /* doubles snum */
+d = j % 10; /* singles snum */
+
+if (a < c || (a == c && b < d))
 {
-putchar(',');
-putchar(' ');
+putchar(a + '0');
+putchar(b + '0');
+putchar(32);
+putchar(c + '0');
+putchar(d + '0');
+
+if (!(a == 9 && b == 8))
+{
+putchar(44);
+putchar(32);
 }
 }
 }
-putchar('\n');
+}
+putchar(10);
+
 return (0);
 }
