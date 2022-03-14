e5d72f63cb460fad27fe98593327ee49f4061792
chidolight7 <light.odazie@yahoo.com>
2022-03-14 06:14:19 -0700

    task10

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,15 +7,12 @@ int main(void)
 {
 int i;
 
-for (i = '0'; i <= '9'; i++)	
+for (i = 0; i <= 9; i++)	
 {
 
 putchar(i);
-if (i == '9')
+if (i == 9)
 {
-break ;
-}
-
 putchar(',');
 putchar(' ');
 }
