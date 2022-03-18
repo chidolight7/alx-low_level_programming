581aed5b8547037fa545b4f135b6d85ac007e240
chidolight7 <light.odazie@yahoo.com>
2022-03-18 16:39:08 -0700

    task2b

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _isupper - evalue if letter is uppercase .
+ *oc: print int
+ * Return: Always 0.
+ */
+
+int _isupper(int c)
+{
+if (c >= 'A' && c <= '2')
+}
+return (1);
+}
+
+else{
+{
+return (0);
+}
+
+}
+
